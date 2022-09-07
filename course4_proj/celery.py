a67ba340ab3bb59c7736eb54b6bb387533ac5eba
Nasr Elsisi <nasrelsisi00@gmail.com>
2022-09-07 19:21:37 +0000

    Finish scheduling with celery

--- course4_proj/celery.py
+++ course4_proj/celery.py
@@ -14,3 +14,10 @@ app = Celery("course4_proj")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+# https://docs.celeryq.dev/en/stable/userguide/periodic-tasks.html
+# https://github.com/celery/celery/tree/master/celery
+# https://github.com/celery/django-celery-beat/tree/master/django_celery_beat
+# https://github.com/celery/django-celery-beat/blob/master/django_celery_beat/models.py
+# https://github.com/celery/django-celery-beat/blob/d4b97af62889527582194f3932e013ad972a88b3/django_celery_beat/schedulers.py
+# https://github.com/celery/django-celery-results/tree/master/django_celery_results
+# https://crontab.guru/
